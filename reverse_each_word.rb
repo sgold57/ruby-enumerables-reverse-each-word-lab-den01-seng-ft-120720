a152347c452c520446952d2f85e9af90af0a65c1
Sam Gold <sgold5757@gmail.com>
2020-10-16 23:25:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,5 @@ require 'pry'
 
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
-  sentence_array.collect do |word|
-    word.reverse
-    binding.pry
-  end
-  sentence_array.join(" ")
+  sentence_array.collect {|word| word.reverse}
 end
