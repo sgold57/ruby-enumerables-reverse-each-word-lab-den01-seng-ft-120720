f6e93b1138a4ec8c223484bcf1f53b817e2d59aa
Sam Gold <sgold5757@gmail.com>
2020-10-16 23:29:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,5 @@ require 'pry'
 
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
-  sentence_array.collect {|word| word.reverse}
+  sentence_array.collect{|word| word.reverse}.join(" ")
 end
