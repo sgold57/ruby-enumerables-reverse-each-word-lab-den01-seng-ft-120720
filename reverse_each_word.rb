dfe0c3e9ebfc0a206cc9dd9bacbaf9a5ef10ec43
Sam Gold <sgold5757@gmail.com>
2020-10-16 23:18:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
   reverse_array = []
-  sentence.array.each do |word|
+  sentence_array.each do |word|
     reverse_array << word.reverse
   end
   reverse_array
